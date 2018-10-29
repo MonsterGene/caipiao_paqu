56feffc3a4a33c97564a19abcdb2468f50ff0f3f
Monster <monster.j.zhang@mail.foxconn.com>
2018-10-29 18:08:28 +0800

    增加期号为唯一索引

--- db/models/kaijiang.js
+++ db/models/kaijiang.js
@@ -2,9 +2,13 @@ const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
 	caipiao_pinzhong: String, //彩种
-	expect: String,  // 期号
+	expect: { // 期号
+		type: String,
+		unique: true // 设为唯一性索引
+	},
+	sortedopenncode: String, // 排序后的开奖结果
 	opencode: String, // 开奖结果
-	opentime: Date,
+	opentime: Date, // 开奖时间
 	cpName: String,
 	meta: {
 		createAt: {type:Date, default:Date.now()}
